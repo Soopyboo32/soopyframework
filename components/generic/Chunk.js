efad5d378698d2533ea3dcf341ec3b9e74fc6b5d
Soopyboo32 <49228220+Soopyboo32@users.noreply.github.com>
2024-01-18 07:01:59 +0800

    chunk

---
+++ components/generic/Chunk.js
@@ -0,0 +1,28 @@
+import {html, staticCss, thisClass, useRef} from "../../helpers.js";
+
+let chunkCss = staticCss.named("chapterChunk").css`${thisClass} {
+    content-visibility: visible;
+    contain-intrinsic-size: auto 0;
+}`
+
+export function Chunk(elms = [], chunkSize = 50) {
+    const observer = new IntersectionObserver(entries => {
+        for (let e of entries) {
+            e.target.style.contentVisibility = e.isIntersecting ? "auto" : "hidden";
+        }
+    });
+
+    let chunks = [[[], useRef().observe(observer)]];
+    elms.forEach((c, i) => {
+        if (i && i % chunkSize === 0) chunks.push([[], useRef().observe(observer)])
+        chunks[chunks.length - 1][0].push(c)
+    })
+
+    return html`
+		${chunks.map(([chunk, ref]) => html`
+			<div ${chunkCss} ${ref}>
+				${chunk.join("")}
+			</div>
+		`).join("")}
+    `
+}
--- helpers.js
+++ helpers.js
@@ -13,6 +13,7 @@
  * @property {() => boolean} exists
  * @property {() => HTMLElement | null} getElm
  * @property {(callback: () => any) => Reference} onRemove
+ * @property {(observer: IntersectionObserver) => Reference} observe
  */
 
 /**
@@ -23,121 +24,125 @@
  * @returns {Reference}
  */
 export function useRef() {
-	let id = generateId();
+    let id = generateId();
 
-	let removeCb = [];
-	let intervals = [];
-	let timeouts = [];
+    let removeCb = [];
+    let intervals = [];
+    let timeouts = [];
 
-	/**
-	 * @type {Reference}
-	 */
-	let ref = {
-		toString: () => `id="${id}"`,
-		onClick: (callback) => {
-			onEventRaw(ref, "click", callback);
-			return ref;
-		},
-		onEnterKey: (callback) => {
-			onEventRaw(ref, "keyup", (key) => {
-				if (key.key === "Enter") {
-					callback();
-				}
-			});
-			return ref;
-		},
-		onHoverEnter: (callback) => {
-			onEventRaw(ref, "mouseenter", callback);
-			return ref;
-		},
-		onHoverMove: (callback) => {
-			onEventRaw(ref, "mousemove", callback);
-			return ref;
-		},
-		onHoverExit: (callback) => {
-			onEventRaw(ref, "mouseleave", callback);
-			return ref;
-		},
-		reRender: (data) => {
-			let elm = ref.getElm();
-			if (!elm) return ref;
-			elm.outerHTML = data;
-			return ref;
-		},
-		renderInner: (data) => {
-			let elm = ref.getElm();
-			if (!elm) return ref;
-			elm.innerHTML = data;
-			return ref;
-		},
-		exists: () => {
-			let elm = ref.getElm();
-			return !!elm;
-		},
-		getElm: () => document.getElementById(id),
-		onRemove: (callback) => {
-			if (!removeCb.length) {
-				let interval = setInterval(() => {
-					if (ref.exists()) {
-						return;
-					}
+    /**
+     * @type {Reference}
+     */
+    let ref = {
+        toString: () => `id="${id}"`,
+        onClick: (callback) => {
+            onEventRaw(ref, "click", callback);
+            return ref;
+        },
+        onEnterKey: (callback) => {
+            onEventRaw(ref, "keyup", (key) => {
+                if (key.key === "Enter") {
+                    callback();
+                }
+            });
+            return ref;
+        },
+        onHoverEnter: (callback) => {
+            onEventRaw(ref, "mouseenter", callback);
+            return ref;
+        },
+        onHoverMove: (callback) => {
+            onEventRaw(ref, "mousemove", callback);
+            return ref;
+        },
+        onHoverExit: (callback) => {
+            onEventRaw(ref, "mouseleave", callback);
+            return ref;
+        },
+        reRender: (data) => {
+            let elm = ref.getElm();
+            if (!elm) return ref;
+            elm.outerHTML = data;
+            return ref;
+        },
+        renderInner: (data) => {
+            let elm = ref.getElm();
+            if (!elm) return ref;
+            elm.innerHTML = data;
+            return ref;
+        },
+        exists: () => {
+            let elm = ref.getElm();
+            return !!elm;
+        },
+        getElm: () => document.getElementById(id),
+        onRemove: (callback) => {
+            if (!removeCb.length) {
+                let interval = setInterval(() => {
+                    if (ref.exists()) {
+                        return;
+                    }
 
-					clearInterval(interval);
-					for (let cb of removeCb) {
-						cb();
-					}
-				}, 1000);
+                    clearInterval(interval);
+                    for (let cb of removeCb) {
+                        cb();
+                    }
+                }, 1000);
 
-			}
-			removeCb.push(callback);
-			return ref;
-		},
-		css: (...args) => {
-			let elm = ref.getElm();
-			if (!elm) return ref;
+            }
+            removeCb.push(callback);
+            return ref;
+        },
+        css: (...args) => {
+            let elm = ref.getElm();
+            if (!elm) return ref;
 
-			if (args[0]._classType === "StaticCss") {
-				ref.getElm().className = args[0].getAllClasses().join(" ");
-				return ref;
-			}
+            if (args[0]._classType === "StaticCss") {
+                ref.getElm().className = args[0].getAllClasses().join(" ");
+                return ref;
+            }
 
-			elm.style = toCssString(args);
+            elm.style = toCssString(args);
 
-			return ref;
-		},
-		interval: (callback, timeout) => {
-			if (!intervals.length) {
-				ref.onRemove(() => {
-					for (let id of intervals) {
-						clearInterval(id);
-					}
-				});
-			}
+            return ref;
+        },
+        interval: (callback, timeout) => {
+            if (!intervals.length) {
+                ref.onRemove(() => {
+                    for (let id of intervals) {
+                        clearInterval(id);
+                    }
+                });
+            }
 
-			intervals.push(setInterval(() => {
-				if (!ref.exists()) return;
-				callback();
-			}, timeout));
-			return ref;
-		},
-		timeout: (callback, timeout) => {
-			if (!timeouts.length) {
-				ref.onRemove(() => {
-					for (let id of timeouts) {
-						clearTimeout(id);
-					}
-				});
-			}
+            intervals.push(setInterval(() => {
+                if (!ref.exists()) return;
+                callback();
+            }, timeout));
+            return ref;
+        },
+        timeout: (callback, timeout) => {
+            if (!timeouts.length) {
+                ref.onRemove(() => {
+                    for (let id of timeouts) {
+                        clearTimeout(id);
+                    }
+                });
+            }
 
-			timeouts.push(setTimeout(() => {
-				if (!ref.exists()) return;
-				callback();
-			}, timeout));
-			return ref;
-		}
-	};
+            timeouts.push(setTimeout(() => {
+                if (!ref.exists()) return;
+                callback();
+            }, timeout));
+            return ref;
+        },
+        observe: (observer) => {
+            setTimeout(() => ref.exists() && observer.observe(ref.getElm()))
+            return ref;
+        }
+    };
 
-	return ref;
+    return ref;
 }
 
 /**
@@ -145,32 +150,32 @@ export function useRef() {
  * @param callback
  */
 function onEventRaw(ref, event, callback) {
-	let elm = ref.getElm();
-	if (elm) {
-		elm.addEventListener(event, (...args) => {
-			callback(...args);
-		});
-		return;
-	}
+    let elm = ref.getElm();
+    if (elm) {
+        elm.addEventListener(event, (...args) => {
+            callback(...args);
+        });
+        return;
+    }
 
-	setTimeout(() => {
-		let elm = ref.getElm();
-		if (!elm) return;
-		elm.addEventListener(event, (...args) => {
-			callback(...args);
-		});
-	});
+    setTimeout(() => {
+        let elm = ref.getElm();
+        if (!elm) return;
+        elm.addEventListener(event, (...args) => {
+            callback(...args);
+        });
+    });
 }
 
 /**
  * @returns {string}
  */
 function toHtmlString(asd) {
-	let css = "";
-	asd[0].forEach((s, i) => {
-		css += s + (asd[i + 1] || "");
-	});
-	return css;
+    let css = "";
+    asd[0].forEach((s, i) => {
+        css += s + (asd[i + 1] || "");
+    });
+    return css;
 }
 
 /**
@@ -179,7 +184,7 @@ function toHtmlString(asd) {
  * @returns HTML
  */
 export function html(...args) {
-	return toHtmlString(args);
+    return toHtmlString(args);
 }
 
 /**
@@ -187,38 +192,38 @@ export function html(...args) {
  */
 
 export function css(...args) {
-	let str = toCssString(args);
+    let str = toCssString(args);
 
-	return fromCssString(str);
+    return fromCssString(str);
 }
 
 function toCssString(asd) {
-	let css = "";
-	asd[0].forEach((s, i) => {
-		if ((typeof asd[i + 1] == "object" || typeof asd[i + 1] == "function")
-			&& (asd[i + 1]._classType === "DynamicCss")) {
-			css += s + asd[i + 1].getRawCss();
-		} else {
-			css += s + (asd[i + 1] || "");
-		}
-	});
-	return css;
+    let css = "";
+    asd[0].forEach((s, i) => {
+        if ((typeof asd[i + 1] == "object" || typeof asd[i + 1] == "function")
+            && (asd[i + 1]._classType === "DynamicCss")) {
+            css += s + asd[i + 1].getRawCss();
+        } else {
+            css += s + (asd[i + 1] || "");
+        }
+    });
+    return css;
 }
 
 function fromCssString(str) {
-	let val = str;
+    let val = str;
 
-	/**@type {Css} */
-	let ret = (...args2) => {
-		let css = toCssString(args2);
+    /**@type {Css} */
+    let ret = (...args2) => {
+        let css = toCssString(args2);
 
-		return fromCssString(val + css);
-	};
-	ret.toString = () => `style="${val.replace("\"", "\\\"").replace(/[\n\t]/g, "").replace(/;;+/g, ";")}"`;
-	ret.getRawCss = () => val;
-	ret._classType = "DynamicCss";
+        return fromCssString(val + css);
+    };
+    ret.toString = () => `style="${val.replace("\"", "\\\"").replace(/[\n\t]/g, "").replace(/;;+/g, ";")}"`;
+    ret.getRawCss = () => val;
+    ret._classType = "DynamicCss";
 
-	return ret;
+    return ret;
 }
 
 /**
@@ -236,106 +241,106 @@ function fromCssString(str) {
  */
 
 export function staticCss(...args) {
-	let data = toStaticCssData(args);
+    let data = toStaticCssData(args);
 
-	return fromStaticCssData(data, [generateClassName()]);
+    return fromStaticCssData(data, [generateClassName()]);
 }
 
 staticCss.named = (id) => {
-	return fromStaticCssData([], [], id);
+    return fromStaticCssData([], [], id);
 };
 
 staticCss.css = (...args) => {
-	let data = toStaticCssData(args);
+    let data = toStaticCssData(args);
 
-	return fromStaticCssData(data, [generateClassName()]);
+    return fromStaticCssData(data, [generateClassName()]);
 };
 
 function toStaticCssData(asd) {
-	let css = [];
-	asd[0].forEach((s, i) => {
-		css.push(s);
-		if ((typeof asd[i + 1] == "object" || typeof asd[i + 1] == "function")
-			&& (
-				asd[i + 1].internal_isCssClassGetter === true
-				|| asd[i + 1].internal_isCssClassUUIDGetter === true
-				|| asd[i + 1]._classType === "StaticCss"
-			)) {
-			css.push(asd[i + 1]);
-		} else if (asd[i + 1]) {
-			css.push(asd[i + 1] + "");
-		}
-	});
-	if (css[0].startsWith("{") && css[css.length - 1].endsWith("}")) {
-		css[0] = css[0].substring(1, css[0].length);
-		css[css.length - 1] = css[css.length - 1].substring(0, css[css.length - 1].length - 1);
-	}
-	return css;
+    let css = [];
+    asd[0].forEach((s, i) => {
+        css.push(s);
+        if ((typeof asd[i + 1] == "object" || typeof asd[i + 1] == "function")
+            && (
+                asd[i + 1].internal_isCssClassGetter === true
+                || asd[i + 1].internal_isCssClassUUIDGetter === true
+                || asd[i + 1]._classType === "StaticCss"
+            )) {
+            css.push(asd[i + 1]);
+        } else if (asd[i + 1]) {
+            css.push(asd[i + 1] + "");
+        }
+    });
+    if (css[0].startsWith("{") && css[css.length - 1].endsWith("}")) {
+        css[0] = css[0].substring(1, css[0].length);
+        css[css.length - 1] = css[css.length - 1].substring(0, css[css.length - 1].length - 1);
+    }
+    return css;
 }
 
 function fromStaticCssData(data, classes = [], nextName) {
-	classes = [...new Set(classes)];
-	let val = data;
+    classes = [...new Set(classes)];
+    let val = data;
 
-	/**@type {StaticCss} */
-	let ret = (...args2) => {
-		let css = toStaticCssData(args2);
+    /**@type {StaticCss} */
+    let ret = (...args2) => {
+        let css = toStaticCssData(args2);
 
-		let className = generateClassName(nextName);
-		return fromStaticCssData(css, [...classes, className]);
-	};
-	ret.css = (...args2) => {
-		let css = toStaticCssData(args2);
+        let className = generateClassName(nextName);
+        return fromStaticCssData(css, [...classes, className]);
+    };
+    ret.css = (...args2) => {
+        let css = toStaticCssData(args2);
 
-		let className = generateClassName(nextName);
-		return fromStaticCssData(css, [...classes, className]);
-	};
-	ret.merge = (otherCss) => {
-		return fromStaticCssData([], [...classes, ...otherCss.getAllClasses(), ""]);
-	};
-	ret.named = (id) => {
-		nextName = id;
-		return ret;
-	};
-	ret.getCss = () => val.map(d => {
-		if (d.internal_isCssClassGetter) return `.${classes[classes.length - 1]}`;
-		if (d.internal_isCssClassUUIDGetter) return `${classes[classes.length - 1]}`;
-		if (d._classType === "StaticCss") return `.${d.getClassName()}`;
+        let className = generateClassName(nextName);
+        return fromStaticCssData(css, [...classes, className]);
+    };
+    ret.merge = (otherCss) => {
+        return fromStaticCssData([], [...classes, ...otherCss.getAllClasses(), ""]);
+    };
+    ret.named = (id) => {
+        nextName = id;
+        return ret;
+    };
+    ret.getCss = () => val.map(d => {
+        if (d.internal_isCssClassGetter) return `.${classes[classes.length - 1]}`;
+        if (d.internal_isCssClassUUIDGetter) return `${classes[classes.length - 1]}`;
+        if (d._classType === "StaticCss") return `.${d.getClassName()}`;
 
-		return d;
-	}).join("");
-	ret.getClassName = () => classes[classes.length - 1];
-	ret.getAllClasses = () => classes;
-	ret.toString = () => {
-		return `class="${classes.join(" ")}"`;
-	};
-	ret._classType = "StaticCss";
+        return d;
+    }).join("");
+    ret.getClassName = () => classes[classes.length - 1];
+    ret.getAllClasses = () => classes;
+    ret.toString = () => {
+        return `class="${classes.join(" ")}"`;
+    };
+    ret._classType = "StaticCss";
 
-	addCssToFile(ret);
+    addCssToFile(ret);
 
-	return ret;
+    return ret;
 }
 
 export let thisClass = {
-	internal_isCssClassGetter: true,
-	uuid: {
-		internal_isCssClassUUIDGetter: true,
-	}
+    internal_isCssClassGetter: true,
+    uuid: {
+        internal_isCssClassUUIDGetter: true,
+    }
 };
 
 let generatedIds = new Set([""]);
 
 function generateClassName(id = "") {
-	let testId = id;
-	while (generatedIds.has(testId)) {
-		testId = id + "_" + Math.floor(Math.random() * 2400000).toString(16);
-	}
-	generatedIds.add(testId);
-	return testId;
+    let testId = id;
+    while (generatedIds.has(testId)) {
+        testId = id + "_" + Math.floor(Math.random() * 2400000).toString(16);
+    }
+    generatedIds.add(testId);
+    return testId;
 }
 
 function generateId() {
-	return Math.floor(Math.random() * 2400000).toString(16);
+    return Math.floor(Math.random() * 2400000).toString(16);
 }
 
 let styleElm = document.getElementById("css");
@@ -345,20 +350,20 @@ let cssAdded = new Set([]);
  * @param {StaticCss} staticCss
  */
 function addCssToFile(staticCss) {
-	if (cssAdded.has(staticCss.getClassName())) {
-		return;
-	}
-	cssAdded.add(staticCss.getClassName());
+    if (cssAdded.has(staticCss.getClassName())) {
+        return;
+    }
+    cssAdded.add(staticCss.getClassName());
 
-	styleElm.innerHTML += staticCss.getCss() + "\n";
+    styleElm.innerHTML += staticCss.getCss() + "\n";
 }
 
 /**
  * @param {number} x
  */
 export function numberWithCommas(x) {
-	if (x === undefined) return "";
-	let parts = x.toString().split(".");
-	parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-	return parts.join(".");
+    if (x === undefined) return "";
+    let parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
 }
