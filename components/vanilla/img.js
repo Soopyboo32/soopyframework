f38a8138c2b3a863164fa33c4f9199c58c63ff65
Soopyboo32 <49228220+Soopyboo32@users.noreply.github.com>
2024-01-20 20:34:46 +0800

    ikmage

--- components/vanilla/img.js
+++ components/vanilla/img.js
@@ -1,9 +1,27 @@
-import {html, useRef} from "../../helpers.js";
+import {css, html, useRef} from "../../helpers.js";
 
-export function Image(url, css=undefined) {
+let fixedCss = css;
+
+export function Img(url, {css = undefined, lazyLoad = true, fadeIfLoaded = false} = {}) {
     let imageRef = useRef();
 
+    let image = new Image();
+    image.src = url;
+    let cached = image.complete;
+
+    let shouldFade = fadeIfLoaded || !cached;
+
     return html.withRef(imageRef)`
-		<img src="${url}" ${imageRef} ${css || ""}>
+		<img
+            src="${url}"
+            ${imageRef}
+            ${css || ""}
+            ${shouldFade ? fixedCss`
+                transition: opacity 0.5s;
+                opacity: 0;
+            ` : ""}
+            ${lazyLoad ? `loading="lazy"` : ""}
+            ${shouldFade ? `onload="setTimeout(()=>this.style.opacity=1)"` : ""}
+		>
     `
 }
