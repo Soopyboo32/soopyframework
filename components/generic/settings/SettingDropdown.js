679f4537679f68266dbe87994d1346cfc770844c
Soopyboo32 <49228220+Soopyboo32@users.noreply.github.com>
2024-04-16 00:10:47 +0800

    improve dropdown setting css

--- components/generic/settings/SettingDropdown.js
+++ components/generic/settings/SettingDropdown.js
@@ -1,5 +1,13 @@
-import { html, useRef } from "../../../helpers.js";
+import { html, thisClass, useRef } from "../../../helpers.js";
 import { dropdownCss } from "../../../css.js";
+
+let settingDropdownCss = dropdownCss.named("setting-dropdown").css`${thisClass} {
+	font-size: 1.1em;
+	margin: 0;
+	padding: 2px;
+	height: unset;
+	border-radius: 10px;
+}`
 
 export function SettingDropdown(selected, options, updateFn) {
 	let dropdownRef = useRef().onChange(asd => {
@@ -8,7 +16,7 @@ export function SettingDropdown(selected, options, updateFn) {
 	});
 
 	return html`
-		<select ${dropdownRef} ${dropdownCss}>
+		<select ${dropdownRef} ${settingDropdownCss}>
 			${Object.entries(options).map(([key, value]) => html`
 				<option ${selected === key ? "selected" : ""} value="${key}">${value}</option>
 			`).join("")}
