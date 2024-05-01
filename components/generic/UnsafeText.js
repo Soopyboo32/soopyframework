4aa18c8f52a5ccfd52980376e1e2dec40199eec4
Soopyboo32 <49228220+Soopyboo32@users.noreply.github.com>
2024-05-02 02:37:25 +0800

    Update UnsafeText.js

--- components/generic/UnsafeText.js
+++ components/generic/UnsafeText.js
@@ -1,13 +1,5 @@
-import { html, useRef } from "../../helpers.js";
-
 export function UnsafeText(text) {
-	let ref = useRef();
-
-	setTimeout(() => {
-		if (ref.getElm()) {
-			ref.getElm().innerText = text;
-		}
-	});
-
-	return html`<span ${ref}></span>`;
+	let span = document.createElement("span");
+	span.textContent = text;
+	return span.innerHTML;
 }
