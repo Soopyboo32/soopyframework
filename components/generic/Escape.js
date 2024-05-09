3709a30e9d3282c1c4f9fe2d891171ec14d59390
Soopyboo32 <49228220+Soopyboo32@users.noreply.github.com>
2024-05-09 17:31:00 +0800

    a

--- components/generic/Escape.js
+++
@@ -1,3 +0,0 @@
-export function Escape(str, specialChars = ["\"", "\\"], escapeChar = "\\") {
-	return str.split("").map(c => specialChars.includes(c) ? escapeChar + c : c).join("");
-}
--- components/generic/Popup.js
+++ components/generic/Popup.js
@@ -87,6 +87,7 @@ export function Popup(title, content, onclose = () => 0) {
 	let close = useRef().onClick(closeFn);
 
 	let height = 0;
+	console.log(content(closeFn).toString());
 
 	wrapper.className = wrapperCss.getAllClasses().join(" ");
 	wrapper.innerHTML = html.withRef(ref)`
