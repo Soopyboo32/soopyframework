7af0a6a9305105e1e21bcb446472f0c7ad9465c1
Soopyboo32 <49228220+Soopyboo32@users.noreply.github.com>
2024-05-21 02:59:03 +0800

    fire

--- css.js
+++ css.js
@@ -73,118 +73,122 @@ staticCss`{
 }`;
 
 export let buttonCss = staticCss.named("button").css`{
-	${thisClass} {
-		display: flex;
-		align-items: center;
-		height: 26px;
-		width: max-content;
-		margin: 10px;
-		text-align: center;
-		background-color: ${colors.primary_dark};
-		color: ${colors.text};
-		border: 1px solid transparent;
-		border-radius: 5px;
-		transition: 0.15s;
-		transition-timing-function: ease-in;
-		padding-block: 1px; /* yoinked these 2 from button css in chrome */
-		padding-inline: 6px; /* yoinked these 2 from button css in chrome */
-	}
+    ${thisClass} {
+        display: flex;
+        align-items: center;
+        height: 26px;
+        width: max-content;
+        margin: 10px;
+        text-align: center;
+        background-color: ${colors.primary_dark};
+        color: ${colors.text};
+        border: 1px solid transparent;
+        border-radius: 5px;
+        transition: 0.15s;
+        transition-timing-function: ease-in;
+        padding-block: 1px; /* yoinked these 2 from button css in chrome */
+        padding-inline: 6px; /* yoinked these 2 from button css in chrome */
+    }
 
-	${thisClass} > a {
-		color: ${colors.text}; /* remove link color from <a> tags */
-		text-decoration: none; /* remove underline from <a> tags */
-	}
+    ${thisClass} > a {
+        color: ${colors.text}; /* remove link color from <a> tags */
+        text-decoration: none; /* remove underline from <a> tags */
+    }
 
 
-	${thisClass}:hover {
-		background-color: ${colors.primary_dark_hover};
-		cursor: pointer;
-	}
+    ${thisClass}:hover {
+        background-color: ${colors.primary_dark_hover};
+        cursor: pointer;
+    }
 
-	${thisClass}:disabled {
-		background-color: ${colors.grey};
-	}
+    ${thisClass}:disabled {
+        background-color: ${colors.grey};
+    }
+}`;
+
+export let inlineButtonCss = buttonCss.named("inline-button").css`${thisClass} {
+    display: inline-block;
 }`;
 
 export let textboxCss = staticCss.named("textbox").css`{
-	${thisClass} {
-		height: 26px;
-		margin: 10px;
-		border: 1px solid transparent;
-		border-radius: 5px;
-		background-color: ${colors.primary_dark};
-		color: ${colors.text};
-		padding-left: 10px;
-		transition: 0.15s;
-		transition-timing-function: ease-in;
-		outline: none;
-	}
+    ${thisClass} {
+        height: 26px;
+        margin: 10px;
+        border: 1px solid transparent;
+        border-radius: 5px;
+        background-color: ${colors.primary_dark};
+        color: ${colors.text};
+        padding-left: 10px;
+        transition: 0.15s;
+        transition-timing-function: ease-in;
+        outline: none;
+    }
 
-	${thisClass}::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
-		color: ${colors.primary};
-		opacity: 1; /* Firefox */
-	}
+    ${thisClass}::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+        color: ${colors.primary};
+        opacity: 1; /* Firefox */
+    }
 
-	${thisClass}:-ms-input-placeholder { /* Internet Explorer 10-11 */
-		color: ${colors.primary};
-	}
+    ${thisClass}:-ms-input-placeholder { /* Internet Explorer 10-11 */
+        color: ${colors.primary};
+    }
 
-	${thisClass}::-ms-input-placeholder { /* Microsoft Edge */
-		color: ${colors.primary};
-	}
+    ${thisClass}::-ms-input-placeholder { /* Microsoft Edge */
+        color: ${colors.primary};
+    }
 
-	${thisClass}:focus {
-		background: ${getBg(0)};
-		border: 1px solid ${colors.primary_dark};
-	}
+    ${thisClass}:focus {
+        background: ${getBg(0)};
+        border: 1px solid ${colors.primary_dark};
+    }
 }`;
 
 export let invisibleCss = staticCss.named("invisible").css`${thisClass} {
-	opacity: 0;
+    opacity: 0;
 }`;
 
 export let dropdownCss = staticCss.named("dropdown").css`{
-	${thisClass} {
-		height: 23px;
-		margin: 10px;
-		border: 1px solid transparent;
-		border-radius: 5px;
-		background-color: ${colors.primary_dark};
-		color: ${colors.text};
-		outline: none;
-	}
+    ${thisClass} {
+        height: 23px;
+        margin: 10px;
+        border: 1px solid transparent;
+        border-radius: 5px;
+        background-color: ${colors.primary_dark};
+        color: ${colors.text};
+        outline: none;
+    }
 
-	/*TODO: fix*/
+    /*TODO: fix*/
 
-	${thisClass} > option:hover {
-		background-color: ${colors.primary};
-	}
+    ${thisClass} > option:hover {
+        background-color: ${colors.primary};
+    }
 }`;
 
 export let checkboxCss = staticCss.named("checkbox").css`{
-	${thisClass} {
-		-webkit-appearance: none;
-		appearance: none;
-		/*background-color: #fff;*/
-		color: ${colors.primary};
-		width: 1.5em;
-		height: 1.5em;
-		border: 0.15em solid currentColor;
-		border-radius: 5px;
-		display: inline-flex;
-		justify-content: space-evenly;
-		align-items: center;
-		transition: 0.1s;
-	}
+    ${thisClass} {
+        -webkit-appearance: none;
+        appearance: none;
+        /*background-color: #fff;*/
+        color: ${colors.primary};
+        width: 1.5em;
+        height: 1.5em;
+        border: 0.15em solid currentColor;
+        border-radius: 5px;
+        display: inline-flex;
+        justify-content: space-evenly;
+        align-items: center;
+        transition: 0.1s;
+    }
 
-	${thisClass}:checked {
-		background-color: ${colors.primary};
-	}
+    ${thisClass}:checked {
+        background-color: ${colors.primary};
+    }
 
-	${thisClass}:checked::after {
-		color: white;
-		font-size: 1.3em;
-		content: "✔";
-		transform: translateY(-1px);
-	}
+    ${thisClass}:checked::after {
+        color: white;
+        font-size: 1.3em;
+        content: "✔";
+        transform: translateY(-1px);
+    }
 }`;
